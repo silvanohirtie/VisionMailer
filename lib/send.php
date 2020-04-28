d7b086246d47f65de1cd2b82c25b08569be545ad
Silvano H <40663430+silvanohirtie@users.noreply.github.com>
2020-04-28 16:08:23 +0200

    Delete send.php

--- lib/send.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to_email = 'hirtie.silvano@gmail.com';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: noreply @ company . com';
-mail($to_email,$subject,$message,$headers);
-
-?>
